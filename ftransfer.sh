9b5daf46e93eaf99b66dcb7bdd7980443ed007cf
jabhinav <jabhinaviitk@gmail.com>
2023-10-28 20:12:01 -0500

    ftranfer

--- ftransfer.sh
+++ ftransfer.sh
@@ -1,4 +1,9 @@
-scp -r -i ~/.ssh/id_rsa ./*.py abhinav@anton.j"$1".rice.edu:/home/abhinav/prog_synth/my_code/Liblearn
-scp -r -i ~/.ssh/id_rsa ./models/*.py abhinav@anton.j"$1".rice.edu:/home/abhinav/prog_synth/my_code/Liblearn/models
-scp -r -i ~/.ssh/id_rsa ./utils/*.py abhinav@anton.j"$1".rice.edu:/home/abhinav/prog_synth/my_code/Liblearn/utils
-scp -r -i ~/.ssh/id_rsa ./custom_peft abhinav@anton.j"$1".rice.edu:/home/abhinav/prog_synth/my_code/Liblearn/custom_peft
+scp -r -i ~/.ssh/id_rsa ./*.py abhinav@anton.j"$1".rice.edu:/home/abhinav/prog_synth/Liblearn/
+scp -r -i ~/.ssh/id_rsa ./utils/*.py abhinav@anton.j"$1".rice.edu:/home/abhinav/prog_synth/Liblearn/utils/
+scp -r -i ~/.ssh/id_rsa ./custom_peft/*.py abhinav@anton.j"$1".rice.edu:/home/abhinav/prog_synth/Liblearn/custom_peft/
+scp -r -i ~/.ssh/id_rsa ./custom_peft/utils/*.py abhinav@anton.j"$1".rice.edu:/home/abhinav/prog_synth/Liblearn/custom_peft/utils/
+
+#scp -r -i ~/.ssh/id_rsa ./*.py paperspace@209.51.170.18:/home/paperspace/LibraryLearning
+#scp -r -i ~/.ssh/id_rsa ./models/*.py paperspace@209.51.170.18:/home/paperspace/LibraryLearning/models
+#scp -r -i ~/.ssh/id_rsa ./utils/*.py paperspace@209.51.170.18:/home/paperspace/LibraryLearning/utils
+#scp -r -i ~/.ssh/id_rsa ./custom_peft paperspace@184.105.5.246:/home/paperspace/LibraryLearning/custom_peft
